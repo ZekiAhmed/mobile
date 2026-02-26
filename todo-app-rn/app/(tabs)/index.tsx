9fc81c62fae8adfcc88fb7e93f2a1dd4d74b3aa5
Zeki Ahmed <changezworld3@gmail.com>
2026-02-26 04:29:08 +0300

    tabs navigator done

---
+++ todo-app-rn/app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={style.container}>
+      <Text style={style.content}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    fontSize: 20,
+  }
+})
