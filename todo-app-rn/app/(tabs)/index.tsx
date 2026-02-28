4ce2c3dff59ac8446bbd92acd9dc77aa2590c03b
Zeki Ahmed <changezworld3@gmail.com>
2026-03-01 02:25:31 +0300

    styles files are created

--- todo-app-rn/app/(tabs)/index.tsx
+++ todo-app-rn/app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
-import useTheme from "@/hooks/useTheme";
+import { api } from "@/convex/_generated/api";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+
+  const styles = createStyles(colors);
   return (
-    <View style={style.container}>
-      <Text style={style.content}>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle the mode</Text>
       </TouchableOpacity>
@@ -13,14 +17,19 @@ export default function Index() {
   );
 }
 
+const createStyles = (colors:ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      gap:10,
+      backgroundColor:colors.bg,
+    },
+    content: {
+      fontSize: 20,
+    }
+  })
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  content: {
-    fontSize: 20,
-  }
-})
+  return styles
+}
